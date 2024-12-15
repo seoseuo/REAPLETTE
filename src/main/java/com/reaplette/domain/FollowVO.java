ee7bbc79af750079d1ce2d86876fbaedafc99100
gunho <rjsghsky@gmail.com>
2024-12-16 00:22:07 +0900

    no message

--- src/main/java/com/reaplette/domain/FollowVO.java
+++
@@ -1,9 +0,0 @@
-package com.reaplette.domain;
-
-import lombok.Data;
-
-@Data
-public class FollowVO {
-    private String followingId; // 팔로잉하는 사용자 ID
-    private String followerId;  // 팔로우하는 사용자 ID
-}
